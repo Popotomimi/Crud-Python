235ca135d80282e48be8fb81039d602c116067b7
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2026-02-15 12:59:59 -0300

    First Commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+from models.task import Task
+
+app = Flask(__name__)
+
+tasks = []
+task_id_control = 1
+
+@app.route("/tasks", methods=["POST"])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control, title=data["title"], description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Task created successfully"}), 201
+
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+    output = {
+        "tasks": {
+            "tasks": task_list,
+            "total_tasks": len(task_list)
+        }
+    }
+
+    return jsonify(output), 200
+
+@app.route("/tasks/<int:id>", methods=["GET"])
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict()), 200
+    return jsonify({"message": "Task not found"}), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
